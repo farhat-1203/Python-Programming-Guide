15f95bc0b4c46a7794ebe5094ccec678bf0601b8
Farhat Momin <143961099+farhat-1203@users.noreply.github.com>
2024-12-31 04:11:43 +0530

    Add files via upload

---
+++ num-ex.py
@@ -0,0 +1,26 @@
+
+print("---Exercize 1---")
+field_length=92
+field_width=48.8
+total_area=field_length*field_width
+print("Total area of the field is: ",total_area)
+
+
+print("\n---Exercize 2---")
+chips_bought=9
+cost_per_chip=1.49
+money_paid=20
+total_cost= chips_bought* cost_per_chip
+money_back=money_paid-total_cost
+print("Shopkeeper will give: ",money_back)
+
+
+print("\n---Exercize 3---")
+tiles_length=5.5
+area=tiles_length**2
+cost=area*500
+print("Total cost for bathroom tiles replacement:",cost)
+
+
+print("\n---Exercize 4---")
+print("Binary representation of number 17 is:", format(17,"b") )
