fcfd090cf75d68a83f50725f269bcc727507a812
Farhat Momin <143961099+farhat-1203@users.noreply.github.com>
2024-12-31 04:12:57 +0530

    Add files via upload

---
+++ var-ex.py
@@ -0,0 +1,18 @@
+
+print("---Exercize 1---")
+# cannot create a varibale named 'break'
+
+print("\n---Exercize 2---")
+birth_year=2005
+current_year=2024
+my_age=current_year-birth_year
+print("My age is: ",my_age)
+
+
+print("\n---Exercize 3---")
+first_name= "Farhat Fatimah "
+middle_name= "Abdul Lateef"
+last_name= "Momin"
+full_name= first_name +" "+ middle_name+" " + last_name
+print("My full name is: ",full_name)
+
